ce065397d946631b2e31e5de1d7259bcf3d8d639
Alex Kaufman <alexkaufman06@gmail.com>
2017-09-12 22:14:32 -0700

    Set up react router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Router, browserHistory} from 'react-router'
+import Routes from './routes'
 
-ReactDOM.render(<div><h1>TEST</h1></div>, document.getElementById('root'));
+ReactDOM.render(
+	<Router
+	  history={browserHistory}
+	  routes={Routes}
+	/>,
+	document.getElementById('root')
+)
 // registerServiceWorker();
