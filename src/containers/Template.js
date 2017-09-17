bc43909d18540562584bb6e80367a17f47c074e6
Alex Kaufman <alexkaufman06@gmail.com>
2017-09-17 14:23:26 -0700

    Add Material-UI

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,18 +1,35 @@
 import React, {Component} from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import PropTypes from 'prop-types';
+import RaisedButton from 'material-ui/RaisedButton';
+
+injectTapEventPlugin()
 
 class Template extends Component {
 	render() {
 		return (
-			<div>
-				<header>
-					<h1>TicTacTuring</h1>
-				</header>
-				<main>
-					{this.props.children}
-				</main>
-			</div>
+			<MuiThemeProvider>
+				<div>
+					<header>
+						<h1>TicTacTuring</h1>
+						<RaisedButton
+							label={'Test Button'}
+							primary={true}
+							onTouchTap={()=>{console.log('Hello, I work!')}}
+						/>
+					</header>
+					<main>
+						{this.props.children}
+					</main>
+				</div>
+			</MuiThemeProvider>
 		)
 	}
+}
+
+Template.propTypes = {
+	children: PropTypes.element.isRequired
 }
 
 export default Template
