75f93e55404ec366a4bb145f1d829876c84f73e8
Alex Kaufman <alexkaufman06@gmail.com>
2017-09-24 17:04:29 -0700

    Add a nav drawer

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import PropTypes from 'prop-types';
-import RaisedButton from 'material-ui/RaisedButton';
+import PropTypes from 'prop-types'
+import NavDrawer from '../components/NavDrawer.js'
 
 injectTapEventPlugin()
 
@@ -11,14 +11,7 @@ class Template extends Component {
 		return (
 			<MuiThemeProvider>
 				<div>
-					<header>
-						<h1>TicTacTuring</h1>
-						<RaisedButton
-							label={'Test Button'}
-							primary={true}
-							onTouchTap={()=>{alert('Hello, I work!')}}
-						/>
-					</header>
+					<NavDrawer/>
 					<main>
 						{this.props.children}
 					</main>
